1519d7496988f1e83ff81ecaf8395b4b374115c4
ALEX <sg014877@gmail.com>
2023-11-14 23:54:47 +0200

    test reaction for scroll line

--- wp-content/plugins/listen-message.js
+++ wp-content/plugins/listen-message.js
@@ -3,6 +3,37 @@ window.addEventListener('message', function (event) {
   // Check the origin to ensure it's from the expected parent window
   if (event.origin === 'https://alex.rv.ua') {
       // Display the received message
-      alert('Received message in iframe: ' + event.data);
+      const bodyElement = document.body;
+      const classMainPages = bodyElement.querySelector('.main');
+      const classSecondPages = bodyElement.querySelector('.second');
+      const classThirdPages = bodyElement.querySelector('.third');
+      const windowNotReady = bodyElement.querySelector('.page-not-ready');
+      if (event.data == 20) {
+        if (classMainPages){
+          
+        } if (classSecondPages){
+          
+        } if (classThirdPages){
+          windowNotReady.classList.add('active')
+        } else {
+          console.error('element body hasn`t typePages class')
+        }
+      } if (event.data == 30) {
+
+      } if (event.data == 50) {
+        
+      } if (event.data == 60) {
+        
+      } if (event.data == 70) {
+        
+      } if (event.data == 80) {
+        
+      } if (event.data == 90) {
+        
+      } if (event.data == 100) {
+        
+      } else {
+        console.error('recived MESSAGE from parrent is NOT RIGHT')
+      }
   }
 });
