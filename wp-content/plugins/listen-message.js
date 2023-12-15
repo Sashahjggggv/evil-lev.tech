33f1351a6408f80af8306c9cc554005c99714f2b
ALEX <sg014877@gmail.com>
2023-12-15 20:09:14 +0200

    commit

--- wp-content/plugins/listen-message.js
+++ wp-content/plugins/listen-message.js
@@ -2,7 +2,6 @@ let classMainPages = document.querySelector('.main-page');
 let classSecondPages = document.querySelector('.second-page');
 let classThirdPages = document.querySelector('.third-page');
 let windowNotReady = document.querySelector('.message-page-not-ready');
-console.log('20 20 20 20 20 20 20 20 20')
 
 // Listen for the 'message' event
 window.addEventListener('message', function (event) {
@@ -10,29 +9,34 @@ window.addEventListener('message', function (event) {
   if (event.origin === 'https://alex.rv.ua') {
     // Display the received message
     if (event.data == 20) {
-      console.log('20 20 20 20 20 20 20 20 20')
+      console.log('20')
       if (classMainPages) {
         console.log('main-page')
       } if (classSecondPages) {
         console.log('second-page')
       } if (classThirdPages) {
         console.log('third-page')
-      } else {
-        console.error('page have NOT identifier')
       }
     } if (event.data == 30) {
+      console.log('30')
 
     } if (event.data == 50) {
+      console.log('50')
       
     } if (event.data == 60) {
+      console.log('60')
       
     } if (event.data == 70) {
+      console.log('70')
       
     } if (event.data == 80) {
+      console.log('80')
       
     } if (event.data == 90) {
-      console.log('recived MESSAGE from parrent is 90')
+      console.log('90')
+
     } if (event.data == 100) {
+      console.log('100')
       
     }
   }
