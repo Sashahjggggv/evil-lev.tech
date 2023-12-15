e490099b82bd43666332c912838396d67caa6f69
ALEX <sg014877@gmail.com>
2023-12-15 18:11:16 +0200

    add

--- wp-content/plugins/listen-message.js
+++ wp-content/plugins/listen-message.js
@@ -10,7 +10,7 @@ window.addEventListener('message', function (event) {
       let classThirdPages = bodyElement.querySelector('.third');
       let windowNotReady = bodyElement.querySelector('.page-not-ready');
       if (event.data == 20) {
-        document.getElementsByClassName(".header123").classList.add('none')
+        document.getElementsByClassName(".content-block").classList.add('none')
       } if (event.data == 30) {
 
       } if (event.data == 50) {
