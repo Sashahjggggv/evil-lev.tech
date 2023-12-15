67f34b4226af6443f012df7c32956248b2b9def2
ALEX <sg014877@gmail.com>
2023-12-15 18:49:34 +0200

    add

--- wp-content/plugins/listen-message.js
+++ wp-content/plugins/listen-message.js
@@ -1,33 +1,32 @@
+// let classMainPages = document.body.querySelector('.main');
+// let classSecondPages = document.body.querySelector('.second');
+// let classThirdPages = document.body.querySelector('.third');
+// let windowNotReady = document.body.querySelector('.page-not-ready');
+let header123 = document.body.querySelector('.header123')
+header123.classList.add('none')
+console.log('success.........')
+
 // Listen for the 'message' event
 window.addEventListener('message', function (event) {
   // Check the origin to ensure it's from the expected parent window
-  
   if (event.origin === 'https://alex.rv.ua') {
-      // Display the received message
-      const bodyElement = document.body;
-      let classMainPages = bodyElement.querySelector(`.main`);
-      let classSecondPages = bodyElement.querySelector('.second');
-      let classThirdPages = bodyElement.querySelector('.third');
-      let windowNotReady = bodyElement.querySelector('.page-not-ready');
-      if (event.data == 20) {
-        document.getElementsByClassName(".content-block").classList.add('none')
-      } if (event.data == 30) {
+    // Display the received message
+    if (event.data == 20) {
+      
+    } if (event.data == 30) {
 
-      } if (event.data == 50) {
-        
-      } if (event.data == 60) {
-        
-      } if (event.data == 70) {
-        
-      } if (event.data == 80) {
-        
-      } if (event.data == 90) {
-        console.log('recived MESSAGE from parrent is 90')
-        
-      } if (event.data == 100) {
-        
-      } else {
-        console.error('recived MESSAGE from parrent is NOT RIGHT. event.data is ', event.data)
-      }
+    } if (event.data == 50) {
+      
+    } if (event.data == 60) {
+      
+    } if (event.data == 70) {
+      
+    } if (event.data == 80) {
+      
+    } if (event.data == 90) {
+      console.log('recived MESSAGE from parrent is 90')
+    } if (event.data == 100) {
+      
+    }
   }
 });
